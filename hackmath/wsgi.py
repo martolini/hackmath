1f2d000b4a9d1b66f56215cb837cf1921aa8deb6
Martin Skow Røed <msroed@gmail.com>
2015-08-25 19:37:05 +0200

    Cheeky bug

--- hackmath/wsgi.py
+++ hackmath/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flowsurf.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackmath.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
